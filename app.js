aa7541fdb029e8c87674ac0a14f12eb526c02de2
ClassicZ42 <ClassicZ42@Outlook.com>
2017-11-06 10:57:58 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// lil server
+
+const http = require('http');
+const fs = require('fs');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+// this is the file you are serving // index,html  , if not throws an errrr
+fs.readFile('index.html', (err, html) =>{
+
+  if(err){
+
+      throw err;
+
+  }
+  // da server 
+  const server = http.createServer((req, res) => {
+
+    res.statusCode = 200 ;
+    res.setHeader('Content-type','text/html');
+    res.write(html);
+    res.end();
+  });
+
+
+  server.listen(port , hostname, () => {
+
+  console.log("server started"+port);
+
+  });
+
+});
